66269a58ef14ab5101edde31ca2bb72be63eaae6
Miguel Vaamonde <mvaamfer@gmail.com>
2018-04-03 20:28:36 +0200

    First configuration react + redux

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    message: `Integrating react and redux. Easy ? :)) `,
+  };
+  
+  function reducer(state = initialState) {
+    return state;
+  }
+  
+  export default reducer;
