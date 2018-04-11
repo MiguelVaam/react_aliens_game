c8eda74704a2e156fee6299e8626690722da2f50
Miguel Vaamonde <mvaamfer@gmail.com>
2018-04-11 20:24:27 +0200

    Game with socket Io (with hardcoded score changes). New features: - Secured Auth0 Api to update players scores - Create express server whick will manage real time scores (see server folder) - Socket io client added to use with react (socket.io-client) - New actions to manage players and current player - Reducer updated to manage new actions - Game, App and Canvas udpated to use new props

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,16 @@ export const moveObjects = mousePosition => ({
 export const startGame = () => ({
   type: START_GAME,
 });
+
+export const LEADERBOARD_LOADED = 'LEADERBOARD_LOADED';
+export const LOGGED_IN = 'LOGGED_IN';
+
+export const leaderboardLoaded = players => ({
+  type: LEADERBOARD_LOADED,
+  players
+});
+
+export const loggedIn = player => ({
+  type: LOGGED_IN,
+  player
+});
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -15,17 +15,7 @@ import Leaderboard from './leaderBoard';
 const Canvas = (props) => {
   const gameHeight = 1200;
   const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
-  // Mock data
-  const leaderboard = [
-    { id: 'd4', maxScore: 82, name: 'Ado Kukic', picture: 'https://twitter.com/KukicAdo/profile_image', },
-    { id: 'a1', maxScore: 235, name: 'Bruno Krebs', picture: 'https://twitter.com/brunoskrebs/profile_image', },
-    { id: 'c3', maxScore: 99, name: 'Diego Poza', picture: 'https://twitter.com/diegopoza/profile_image', },
-    { id: 'b2', maxScore: 129, name: 'Jeana Tahnk', picture: 'https://twitter.com/jeanatahnk/profile_image', },
-    { id: 'e5', maxScore: 34, name: 'Jenny Obrien', picture: 'https://twitter.com/jenny_obrien/profile_image', },
-    { id: 'f6', maxScore: 153, name: 'Kim Maida', picture: 'https://twitter.com/KimMaida/profile_image', },
-    { id: 'g7', maxScore: 55, name: 'Luke Oliff', picture: 'https://twitter.com/mroliff/profile_image', },
-    { id: 'h8', maxScore: 146, name: 'Sebastián Peyrott', picture: 'https://twitter.com/speyrott/profile_image', },
-  ];
+  
   return (
     <svg
       id="aliens-go-home-canvas"
@@ -48,7 +38,7 @@ const Canvas = (props) => {
         <g>
           <StartGame onClick={() => props.startGame()} />
           <Title />
-          <Leaderboard currentPlayer={leaderboard[6]} authenticate={signIn} leaderboard={leaderboard} />
+          <Leaderboard currentPlayer={props.currentPlayer} authenticate={signIn} leaderboard={props.players} />
         </g>
       }
 
@@ -77,7 +67,24 @@ Canvas.propTypes = {
     })).isRequired
   }).isRequired,
   trackMouse: PropTypes.func.isRequired,
-  startGame: PropTypes.func.isRequired
+  startGame: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }),
+  players: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    maxScore: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  }))
+};
+
+Canvas.defaultProps = {
+  currentPlayer: null,
+  players: null
 };
 
 export default Canvas;
